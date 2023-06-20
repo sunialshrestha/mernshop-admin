2c3918ded01871067f421d5ed3d9ce3d2a9ea604
sunialshrestha <sunialshrestha@gmail.com>
2023-06-20 22:04:12 +1000

    changing base url of api

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL =  "https://mernshopapi.onrender.com/";
+
+//const BASE_URL = "http://localhost:5000/api/";
 // const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.auth || "{}")?.currentUser?.accessToken;
 
 export const getToken = () => JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.auth || "{}")?.currentUser?.accessToken;
